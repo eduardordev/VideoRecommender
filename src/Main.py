14b163bac4d43b0c1d6943416e1a2e664469a1f8
eduardorh1312 <53280540+eduardorh1312@users.noreply.github.com>
2020-05-24 16:35:04 -0600

    ligeras correcciones en Main.py

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,4 @@
-import database as mp
+import db as mp
 
 opcion = 0
 usuario = ""
@@ -13,44 +13,47 @@ while t:
     while usuario != "":    
         #Se chequea que el usuario exista en la database
         if mp.usuarioexiste(usuario) == True:
-                print('Ingrese la opcion de lo que desea realizar: \n')
-                print("1. Explorar")
-                print("2. Recomendaciones")
-                print("3. Salir")
-                opcion = input("\n")
+    
+            print('Ingrese la opcion de lo que desea realizar: \n')
+            print("1. Explorar")
+            print("2. Recomendaciones")
+            print("3. Salir")
+            opcion = input("\n")
                 
-                if opcion == "1":    
-                    print("Seleccione un video: \n")
-                    listaalmacenada = mp.explorarvideos(usuario)[:]    #slicing para que listaalmacenada sea independiente
+            if opcion == "1":    
+                print("Seleccione un video: \n")
+                listaalmacenada = mp.explorarvideos(usuario)[:]    #slicing para que listaalmacenada sea independiente
                     
-                    for element in listaalmacenada:
-                        print(contador, element)
-                        contador = contador + 1
+                for element in listaalmacenada:
+                    print(contador, element)
+                    contador = contador + 1
                     
-                    opcion = input("\n")
-                    mp.agregarvideo(usuario,listaalmacenada[opcion-1])
+                opcion = input("\n")
+                mp.agregarvideo(usuario,listaalmacenada[opcion-1])
 
 
                     
-                elif opcion == "2":
-                    print('Seleccione un video: \n')
+            elif opcion == "2":
+                print('Seleccione un video: \n')
                     
-                    listaalmacenada = mp.videosrecomendadosusuario(usuario)[:]    #slicing para que listaalmacenada sea independiente
-                    contador =0
+                listaalmacenada = mp.videosrecomendadosusuario(usuario)[:]    #slicing para que listaalmacenada sea independiente
+                contador =0
+                lon = len(listaalmacenada)
                     
-                    for element in listaalmacenada:
-                        while contador <= len(listaalmacenada)
-                            print(contador, element)                
+                    
+                for element in listaalmacenada:
+                    while contador <= lon:
+                        print(contador, element)                
                                     
-                    opcion = input("\n")
-                    mp.agregarvideo(usuario,listaalmacenada[opcion-1])
+                opcion = input("\n")
+                mp.agregarvideo(usuario,listaalmacenada[opcion-1])
 
-                elif opcion == "3":
+            elif opcion == "3":
 
-                    t = False
+                t = False
                     
         else:
-            usuario = "":
+            usuario = ""
             
             
         
