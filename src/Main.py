abbe1659c7cbee6a22d88b0aa6f66a945ba3bcc9
Gabriel Quiroz <46507325+gabrielquirozz@users.noreply.github.com>
2020-05-28 01:33:46 -0600

    Update Main.py
    Arreglo de errores

--- src/Main.py
+++ src/Main.py
@@ -4,59 +4,62 @@ opcion = 0
 usuario = ""
 contador = 1
 t = True
+bandera = True
 
 while t:
-
-    print('Ingresa tu usuario: ')
-    usuario = input()
-
-    while usuario != "":    
-        #Se chequea que el usuario exista en la database
-        if mp.usuarioexiste(usuario) == True:
-    
-            print('Ingrese la opcion de lo que desea realizar: \n')
-            print("1. Explorar")
-            print("2. Recomendaciones")
-            print("3. Salir")
-            opcion = input("\n")
-                
-            if opcion == "1":    
-                print("Seleccione un video: \n")
-                listaalmacenada = mp.explorarvideos(usuario)[:]    #slicing para que listaalmacenada sea independiente
-                    
-                for element in listaalmacenada:
-                    print(contador, element)
-                    contador = contador + 1
-                    
-                opcion = input("\n")
-                mp.agregarvideo(usuario,listaalmacenada[opcion-1])
-
-
-                    
-            elif opcion == "2":
-                print('Seleccione un video: \n')
-                    
-                listaalmacenada = mp.videosrecomendadosusuario(usuario)[:]    #slicing para que listaalmacenada sea independiente
-                contador =0
-                lon = len(listaalmacenada)
-                    
-                    
-                for element in listaalmacenada:
-                    while contador <= lon:
-                        print(contador, element)                
-                                    
-                opcion = input("\n")
-                mp.agregarvideo(usuario,listaalmacenada[opcion-1])
-
-            elif opcion == "3":
-
-                t = False
-                    
-        else:
-            usuario = ""
-            
-            
+    while usuario == "":
+        print('Ingresa tu usuario: ')
+        usuario = input()
+            #Se chequea que el usuario exista en la database
+        if mp.usuarioExiste(usuario) == True:
+            while bandera == True:
         
+                print('Ingrese la opcion de lo que desea realizar: \n')
+                print("1. Explorar")
+                print("2. Recomendaciones")
+                print("3. Historial")
+                print("4. Salir")
+                opcion = input("\n")
+                        
+                if opcion == "1":    
+                    print("Seleccione un video: \n")
+                    listaalmacenada = mp.Explorar(usuario,mp.DevolverNodoV(usuario),mp.DevolverNodoT(usuario,mp.DevolverNodoV(usuario)),mp.DevolverNodoC(usuario,mp.DevolverNodoV(usuario)))[:]    #slicing para que listaalmacenada sea independiente
+                        
+                    for element in listaalmacenada:
+                        print(contador, element)
+                        contador = contador + 1
+                        
+                    opcion = input("\n")
+                    mp.crearRelacion(usuario,listaalmacenada[opcion-1])
+
+
+                            
+                elif opcion == "2":
+                    print('Seleccione un video: \n')
+                        
+                    listaalmacenada = mp.Recomendaciones(usuario,mp.DevolverNodoV(usuario),mp.DevolverNodoT(usuario,mp.DevolverNodoV(usuario)),mp.DevolverNodoC(usuario,mp.DevolverNodoV(usuario)))[:]    #slicing para que listaalmacenada sea independiente
+                    contador =0
+                    lon = len(listaalmacenada)
+                        
+                        
+                    for element in listaalmacenada:
+                        while contador <= lon:
+                            print(contador, element)                
+                                        
+                    opcion = input("\n")
+                    mp.agregarvideo(usuario,listaalmacenada[opcion-1])
+                    
+                elif opcion == "3":
+                    print('Los videos que el usuario ha visto son: \n')
+                    mp.mostrarVideosVistosPorUsuario(usuario)
+
+                elif opcion == "4":
+                    print("Gracias por utilizar el programa")
+                    t = False
+                    bandera = False
+    else:
+        usuario = ""
+          
         
         
         
