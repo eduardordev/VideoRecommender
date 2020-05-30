0a6ae5f1a980f2a4908a00234978167219ee632f
oscarparedez <42584081+oscarparedez@users.noreply.github.com>
2020-05-29 23:52:27 -0600

    Programa finalizado (Falta eliminar historial)

--- src/Main.py
+++ src/Main.py
@@ -3,62 +3,71 @@ import db as mp
 opcion = 0
 usuario = ""
 contador = 1
-t = True
 bandera = True
 
-while t:
-    while usuario == "":
-        print('Ingresa tu usuario: ')
-        usuario = input()
-            #Se chequea que el usuario exista en la database
-        if mp.usuarioExiste(usuario) == True:
-            while bandera == True:
-        
-                print('Ingrese la opcion de lo que desea realizar: \n')
-                print("1. Explorar")
-                print("2. Recomendaciones")
-                print("3. Historial")
-                print("4. Salir")
-                opcion = input("\n")
-                        
-                if opcion == "1":    
-                    print("Seleccione un video: \n")
-                    listaalmacenada = mp.Explorar(usuario,mp.DevolverNodoV(usuario),mp.DevolverNodoT(usuario,mp.DevolverNodoV(usuario)),mp.DevolverNodoC(usuario,mp.DevolverNodoV(usuario)))[:]    #slicing para que listaalmacenada sea independiente
-                        
-                    for element in listaalmacenada:
-                        print(contador, element)
-                        contador = contador + 1
-                        
-                    opcion = input("\n")
-                    mp.crearRelacion(usuario,listaalmacenada[opcion-1])
-
-
-                            
-                elif opcion == "2":
-                    print('Seleccione un video: \n')
-                        
-                    listaalmacenada = mp.Recomendaciones(usuario,mp.DevolverNodoV(usuario),mp.DevolverNodoT(usuario,mp.DevolverNodoV(usuario)),mp.DevolverNodoC(usuario,mp.DevolverNodoV(usuario)))[:]    #slicing para que listaalmacenada sea independiente
-                    contador =0
-                    lon = len(listaalmacenada)
-                        
-                        
-                    for element in listaalmacenada:
-                        while contador <= lon:
-                            print(contador, element)                
-                                        
-                    opcion = input("\n")
-                    mp.agregarvideo(usuario,listaalmacenada[opcion-1])
+while usuario == "":
+    print('INGRESE SU USUARIO: ')
+    usuario = input()
+        #Se chequea que el usuario exista en la database
+    if mp.usuarioExiste(usuario) == True:
+        while bandera == True:
+    
+            print('\nINGRESE UNA OPCION: \n')
+            print("1. Explorar")
+            print("2. Recomendaciones")
+            print("3. Historial")
+            print("4. Salir")
+            opcion = input("\n")
                     
-                elif opcion == "3":
-                    print('Los videos que el usuario ha visto son: \n')
-                    mp.mostrarVideosVistosPorUsuario(usuario)
+            if opcion == "1":
+                contador = 1
+                print("SELECCIONE UN VIDEO POR SU NUMERO: \n")
 
-                elif opcion == "4":
-                    print("Gracias por utilizar el programa")
-                    t = False
-                    bandera = False
-    else:
-        usuario = ""
+                listaAllVideos = mp.allVideos()
+                listaRecomendados = mp.Recomendaciones(usuario)
+                listaHistorial = mp.Historial(usuario)
+                
+                for i in listaRecomendados:
+                    if i in listaAllVideos: listaAllVideos.remove(i)
+                    
+                for i in listaHistorial:
+                    if i in listaAllVideos: listaAllVideos.remove(i)
+
+                
+                for i in listaAllVideos:
+                    print(contador, i)
+                    contador+=1
+
+                opcion = input("\n")
+                mp.crearRelacion(usuario,listaAllVideos[opcion-1])
+                        
+            elif opcion == "2":
+                contador = 1
+                print("SELECCIONE UN VIDEO POR SU NUMERO: \n")
+                    
+                lista = mp.Recomendaciones(usuario)
+                
+                for element in lista:
+                    print(contador, element)
+                    contador+=1
+         
+                opcion = input("\n")
+                mp.agregarvideo(usuario,listaalmacenada[opcion-1])
+                
+            elif opcion == "3":
+                contador = 1
+                print('HISTORIAL DEL USUARIO',usuario.upper(), ':\n')
+                lista = mp.Historial(usuario)
+                
+                for element in lista:
+                    print(contador, element)
+                    contador+=1
+
+            elif opcion == "4":
+                print("GRACIAS POR UTILIZAR EL SISTEMA DE RECOMENDACION DE VIDEOS.")
+                bandera = False
+else:
+    usuario = ""
           
         
         
--- src/db.py
+++ src/db.py
@@ -6,14 +6,6 @@ userName        = "neo4j"
 password        = "123"
 db = Graph(uri, auth=(userName, password), encrypted=False)
 
-
-#Metodo que muestra todos los videos que cierto usuario ha visto
-def mostrarVideosVistosPorUsuario(name):
-        for record in db.run("MATCH (a:user)-[:watched]->(r) " "WHERE a.name = $name " "RETURN r.name", name=name):
-           print(record["r.name"])
-
-#mostrarVideosVistosPorUsuario("Gabriel")
-
 #Metodo que revisa si un usuario existe (Para hacer el login)
 def usuarioExiste(name):
         resultado = db.run("MATCH (a:user) WHERE a.name = $name RETURN a.name", name=name)
@@ -23,52 +15,38 @@ def usuarioExiste(name):
             else:
                 return False
 
-
-#print(usuarioExiste("Oscar"))
             
 #Metodo que crea una relationship entre usuario y video
 def crearRelacion(name, video):
-        db.run("MATCH (a:user),(b:video) WHERE a.name = $name AND b.name = $video CREATE (a)-[:watched]->(b)")
-        
+    db.run("MATCH (a:user),(b:video) WHERE a.name = $name AND b.name = $video CREATE (a)-[:watched]->(b)")
+
+#Explorar
+def Explorar(name):
+    lista = []
+    resultado = db.run("MATCH (a:user)-[:watched]->(b:video), (b)-[:hasTag]->(c:tag), (b)-[:hasGenre]->(d:genre), (e:video)-[:hasGenre]->(d:genre), (e:video)-[:hasTag]->(c:tag) WHERE a.name <> $name RETURN b.name", name=name).data()
+    for line in resultado:
+        lista.append(line.get("b.name"))
+    return lista
 
 #Recomendaciones
-def Recomendaciones(name, video, tag, category):
+def Recomendaciones(name):
     lista = []
-    videos = db.run("MATCH (a:user)-[:watched]->(b:video), (b)-[:hasTag]->(c:tag), (b)-[:hasGenre]->(d:category), (e:video)-[:hasGenre]->(d:category), (e:video)-[:hasTag]->(c:tag) WHERE a.name = $name AND b.name = $video AND c.name = $tag AND d.name = $category RETURN e.name UNION MATCH (a:user)-[:watched]->(b:video), (b)-[:hasGenre]->(d:category), (e:video)-[:hasGenre]->(d:category) RETURN e.name UNION MATCH (a:user)-[:watched]->(b:video), (b)-[:hasGenre]->(d:category), (e:video)-[:hasTag]->(c:tag) RETURN e.name", name=name, video=video, tag=tag, category=category)
-    for line in videos:
-        lista.append(videos["e.name"])
-        return lista
-#Explorar
-def Explorar(name, video, tag, category):
+    resultado = db.run("MATCH (u:user)-[:watched]->(v1), (v1)-[:hasTag]->(t)<-[:hasTag]-(v2), (v1)-[:hasGenre]->(g)<-[:hasGenre]-(v2) WHERE u.name = $name RETURN v2.name", name = name).data()
+    for line in resultado:
+        lista.append(line.get("v2.name"))
+    return lista
+
+#Metodo que muestra todos los videos que cierto usuario ha visto
+def Historial(name):
     lista = []
-    videos = db.run("MATCH (a:user)-[:watched]->(b:video), (b)-[:hasTag]->(c:tag), (b)-[:hasGenre]->(d:category), (e:video)-[:hasGenre]->(d:category), (e:video)-[:hasTag]->(c:tag) WHERE a.name <> $name AND b.name <> $video AND c.name <> $tag AND d.name <> $category RETURN e.name", name=name, video=video, tag=tag, category=category)
-    for line in videos:
-        lista.append(videos["e.name"])
-        return lista
+    for record in db.run("MATCH (a:user)-[:watched]->(r) " "WHERE a.name = $name " "RETURN r.name", name=name):
+        lista.append(record["r.name"])
+    return lista
 
-#Devolver Nodo Video
-def DevolverNodoV(name):
-    resultado = db.run("MATCH (a:user)-[:watched]->(b:video) WHERE a.name = $name RETURN b.name", name=name)
-    for line in resultado:
-          return  line["b.name"]         
-    
+#Todos los videos existentes
+def allVideos():
+    lista = []
+    for record in db.run("MATCH (v:video) return v.name"):
+        lista.append(record["v.name"])
+    return lista
 
-#Devolver Nodo Tag
-def DevolverNodoT(name, video):
-    resultado = db.run("MATCH (a:user)-[:watched]->(b:video), (b)-[:hasTag]->(c:tag) WHERE a.name = $name AND b.name = $video RETURN c.name", name=name, video=video)
-    for line in resultado:
-          return  line["c.name"]
-          
-#Devolver Nodo categoria
-def DevolverNodoC(name, video):
-    resultado = db.run("MATCH (a:user)-[:watched]->(b:video), (b)-[:hasGenre]->(d:category) WHERE a.name = $name AND b.name = $video RETURN d.name", name=name, video=video)
-    for line in resultado:
-          return  line["d.name"]
-        
-#Prueba de los metodos
-"""nombre = "Guido"
-print(DevolverNodoV(nombre))
-print(DevolverNodoT(nombre,DevolverNodoV(nombre)))
-print(DevolverNodoC(nombre,DevolverNodoV(nombre)))
-print(Recomendaciones(nombre, DevolverNodoV(nombre), DevolverNodoT(nombre,DevolverNodoV(nombre)), DevolverNodoC(nombre,DevolverNodoV(nombre))))
-print(Explorar(nombre, DevolverNodoV(nombre), DevolverNodoT(nombre,DevolverNodoV(nombre)), DevolverNodoC(nombr
